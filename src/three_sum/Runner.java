b34ad33461de651ae7ce74076c17ac5aec2219c1
alexanastasyev <alex.anastasyev@mail.ru>
2021-07-08 09:47:18 +0300

    Three sum

---
+++ src/three_sum/Runner.java
@@ -0,0 +1,7 @@
+package three_sum;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(new Solution().threeSum(new int[] {-1,0,1,2,-1,-4}));
+    }
+}
---
+++ src/three_sum/Solution.java
@@ -0,0 +1,37 @@
+package three_sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int low = i + 1;
+                int high = nums.length - 1;
+                int sum = -nums[i];
+                while (low < high) {
+                    if (nums[low] + nums[high] == sum) {
+                        result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        while (low < high && nums[low + 1] == nums[low]) {
+                            low++;
+                        }
+                        while (low < high && nums[high - 1] == nums[high]) {
+                            high--;
+                        }
+                        low++;
+                        high--;
+                    } else if (nums[low] + nums[high] < sum) {
+                        low++;
+                    } else {
+                        high--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
